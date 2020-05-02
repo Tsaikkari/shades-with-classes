c970c1d64eae78fa588cf049806624bdc8c21134
Tsaikkari <trospe@gmx.de>
2020-05-02 22:30:02 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Shades extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      greys: ['one', 'two'],
+      visibility: false
+    };
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      };
+    })
+  }
+  render() {
+    return (
+      <div>
+          <button onClick={this.handleToggleVisibility}>
+            {this.state.visibility ? 'THEY SAY': 'Go back'}
+          </button>
+          {this.state.visibility && <Boxes />}
+          {!this.state.visibility && (
+          <div>
+            {this.state.greys.map((grey) => <p key={grey}>{grey}</p>)}
+          </div>
+          )}
+      </div>
+    );
+  }
+}
+
+class Boxes extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row" id="two-divs-screen">
+            <div className="black s6 ml-auto mr-auto">
+              <p id="left">LIFE IS NOT</p>
+            </div>
+            <div className="white s6 ml-auto mr-auto">
+              <p id="right">BLACK</p>
+              <p className="and">&</p>
+              <p className="wht">WHITE</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Shades />, document.getElementById('root'));
